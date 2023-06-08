644d6d683bfef8127488694d70d7176d4dbcddc1
kolosovpetro <kolosovp94@gmail.com>
2023-06-08 23:07:08 +0200

    lb rule and probe

--- terraform/modules/load_balancer/main.tf
+++ terraform/modules/load_balancer/main.tf
@@ -24,3 +24,23 @@ resource "azurerm_lb_backend_address_pool" "public" {
   loadbalancer_id = azurerm_lb.public.id
   name            = "HRB-NLB-BACKEND"
 }
+
+resource "azurerm_lb_probe" "public" {
+  loadbalancer_id = azurerm_lb.public.id
+  name            = "HttpHealthProbe"
+  port            = 80
+}
+
+resource "azurerm_lb_rule" "public" {
+  loadbalancer_id                = azurerm_lb.public.id
+  name                           = "HttpBalancingRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = var.load_balancer_frontend_ip_configuration_name
+
+  depends_on = [
+    azurerm_lb_probe.public,
+    azurerm_lb_backend_address_pool.public
+  ]
+}
